a5778cc81079b5cf3335603b72764f322c86dd53
Danang Sulistyo <76847578+ViciGG@users.noreply.github.com>
2023-06-10 14:24:46 +0700

    Create index

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Eco Live</title>
+</head>
+
+<body style="background:darkgray;">
+    <div style="color:rgb(252, 252, 252); padding: 30px; font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;">
+        <h1 style="color: blue;">Selamat Datang di Eco Live app</h1>
+        <h3>:)</h3>
+    </div>
+</body>
+
+</html>
