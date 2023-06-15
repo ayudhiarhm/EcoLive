40a58487f567bcd631457dc62194f0585ec7e322
Cyntia Aristhawati Rahman <125940207+cyntiaaristhawatir@users.noreply.github.com>
2023-06-16 06:54:04 +0700

    Update FuncVis.js

--- Front-End Website/src/component/FuncVis.js
+++ Front-End Website/src/component/FuncVis.js
@@ -1,10 +1,13 @@
-import React, { useState } from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import React, { useState } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import ShowChart from "./ShowChart";
 
 function FuncVis() {
-  const [year, setYear] = useState('');
-  const [type, setType] = useState('');
-  const [imageSrc, setImageSrc] = useState('');
+  const [year, setYear] = useState("");
+  const [type, setType] = useState("");
+  const [data, setData] = useState(null);
+  const [label, setLabel] = useState(null);
+  const [imageSrc, setImageSrc] = useState("");
 
   const handleYearChange = (e) => {
     setYear(e.target.value);
@@ -14,33 +17,59 @@ function FuncVis() {
     setType(e.target.value);
   };
 
-  const visualization = (e) => {
-    e.preventDefault();
-
-    // Logic to generate image based on year and type
-    const imageUrl = `https://example.com/images/${year}-${type}.jpg`;
-
-    setImageSrc(imageUrl);
+  const visualization = async (e) => {
+    try {
+      e.preventDefault();
+      const formData = new FormData();
+      formData.append("negara", type.charAt(0).toUpperCase() + type.slice(1));
+      formData.append("tahun", year);
+      let response = await fetch(
+        "https://inimodel-dot-eco-live-389407.et.r.appspot.com/predict",
+        {
+          method: "POST",
+          body: formData,
+        }
+      );
+      response = await response.json();
+      const labels = Array.from(
+        Array(Object.keys(response["CO2 Levels"]).length).keys()
+      );
+      const dataModel = Object.values(response["CO2 Levels"]);
+      setLabel(labels);
+      setData(dataModel);
+    } catch (err) {
+      alert("Negara tidak ada");
+    }
   };
 
   return (
     <div>
       <Container>
-        <Row className='text-center'>
+        <Row className="text-center">
           <form onSubmit={visualization}>
-            <label style={{ marginRight: '10px' }}>
-              Year: 
-            </label>
-            <input type="number" value={year} onChange={handleYearChange} style={{width: '80px'}} />
-            <label style={{ marginLeft: '30px', marginRight: '10px' }}>
+            <label style={{ marginRight: "10px" }}>Year:</label>
+            <input
+              type="number"
+              value={year}
+              onChange={handleYearChange}
+              style={{ width: "80px" }}
+            />
+            <label style={{ marginLeft: "30px", marginRight: "10px" }}>
               Country:
             </label>
             <input type="text" value={type} onChange={handleTypeChange} />
             <br />
-            <button type="submit" className="btn btn-success" style={{ marginTop: '20px' }}>Analyze</button>
+            <button
+              type="submit"
+              className="btn btn-success"
+              style={{ marginTop: "20px" }}
+            >
+              Analyze
+            </button>
           </form>
         </Row>
-        {imageSrc && <img src={imageSrc} alt="Generated Image" />}
+        {data && <ShowChart data={data} labels={label} />}
+        {/* {imageSrc && <img src={imageSrc} alt="Generated Image" />} */}
       </Container>
     </div>
   );
